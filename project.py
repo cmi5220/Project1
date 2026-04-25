29daadf79ac4a29051d5451380154923c3302399
Charvi Iruganti <your_email@example.com>
2026-04-25 17:07:20 -0400

    2

--- project.py
+++ project.py
@@ -8,6 +8,7 @@ def create_board():
 
 
 def print_board(board):
+    """Display the current state of the game board."""
     print("\nCurrent board:")
     print("   0   1   2")
     for row_index, row in enumerate(board):
@@ -15,3 +16,22 @@ def print_board(board):
         if row_index < BOARD_SIZE - 1:
             print("  " + "-" * 9)
     print()
+
+
+def check_winner(board, player):
+    """Return True if the given player has won."""
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    for col in range(BOARD_SIZE):
+        if all(board[row][col] == player for row in range(BOARD_SIZE)):
+            return True
+
+    if all(board[index][index] == player for index in range(BOARD_SIZE)):
+        return True
+
+    if all(board[index][BOARD_SIZE - 1 - index] == player for index in range(BOARD_SIZE)):
+        return True
+
+    return False
